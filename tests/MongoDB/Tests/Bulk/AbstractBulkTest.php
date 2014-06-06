e2c6ca5059e14f17739f226d6ae93e9a54fa8bc5
Jeremy Mikola <jmikola@gmail.com>
2014-06-06 17:52:00 -0400

    Tests for Bulk API method calls

---
+++ tests/MongoDB/Tests/Bulk/AbstractBulkTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace MongoDB\Tests\Bulk;
+
+use MongoDB\Tests\BaseTestCase;
+
+abstract class AbstractBulkTest extends BaseTestCase
+{
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     * @expectedExceptionMessage Missing argument
+     * @see https://jira.mongodb.org/browse/PHP-982
+     */
+    public function testFindRequiresArgument()
+    {
+        $bulk = $this->getBulk();
+        $bulk->find();
+    }
+
+    /**
+     * @expectedException MongoDB\Exception\UnexpectedTypeException
+     */
+    public function testFindRequiresArgumentToBeArrayOrObject()
+    {
+        $bulk = $this->getBulk();
+        $bulk->find(null);
+    }
+
+    /**
+     * @expectedException MongoDB\Exception\UnexpectedTypeException
+     */
+    public function testInsertRequiresArgumentToBeArrayOrObject()
+    {
+        $bulk = $this->getBulk();
+        $bulk->insert(null);
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testRemoveRequiresSelector()
+    {
+        $bulk = $this->getBulk();
+        $bulk->remove(array());
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testRemoveOneRequiresSelector()
+    {
+        $bulk = $this->getBulk();
+        $bulk->removeOne(array());
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUpdateRequiresSelector()
+    {
+        $bulk = $this->getBulk();
+        $bulk->update(array());
+    }
+
+    /**
+     * @expectedException MongoDB\Exception\UnexpectedTypeException
+     */
+    public function testUpdateRequiresArgumentToBeArrayOrObject()
+    {
+        $bulk = $this->getBulk();
+        $bulk->find(array())->update(null);
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUpdateOneRequiresSelector()
+    {
+        $bulk = $this->getBulk();
+        $bulk->updateOne(array());
+    }
+
+    /**
+     * @expectedException MongoDB\Exception\UnexpectedTypeException
+     */
+    public function testUpdateOneRequiresArgumentToBeArrayOrObject()
+    {
+        $bulk = $this->getBulk();
+        $bulk->find(array())->updateOne(null);
+    }
+
+    abstract protected function getBulk();
+}
